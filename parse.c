88b03805fc78670d4d303466e0981e73b77e9875
Tyler Curtis <ekiru.0@gmail.com>
2016-05-14 11:27:53 -0500

    Teach the test parser to parse multiple expressions.

--- parse.c
+++ parse.c
@@ -106,10 +106,17 @@ struct syntax_tree *parse(void) {
 }
 
 int main() {
-	struct syntax_tree *tree = parse();
-	if (tree) {
-		syntax_tree_display(tree);
-	} else {
+	bool parsed_one = false;
+	for (;;) {
+		struct syntax_tree *tree = parse();
+		if (tree) {
+			parsed_one = true;
+			syntax_tree_display(tree);
+		} else {
+			break;
+		}
+	}	
+	if (!parsed_one) {
 		log_error("Couldn't parse a string.");
 	}
 }
