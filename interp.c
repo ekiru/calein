4987cc08a5fbb18a9db23f0ba87353b69a9d0073
Tyler Curtis <ekiru.0@gmail.com>
2016-05-27 20:27:32 -0500

    Add pairs.

--- interp.c
+++ interp.c
@@ -9,6 +9,7 @@ enum value_kind {
 	value_kind_string,
 	value_kind_boolean,
 	value_kind_number,
+	value_kind_pair,
 };
 
 struct value {
@@ -17,6 +18,7 @@ struct value {
 		struct string string;
 		bool boolean;
 		int64_t number;
+		struct value *pair[2];
 	} u;
 };
 
@@ -70,8 +72,35 @@ ARITH_PRIM(mod, %)
 
 #undef ARITH_PRIM
 
-static struct value *primitive_write(const struct action *action) {
-	struct value *v = eval(action->args[0]);
+static struct value *primitive_pair(const struct action *action) {
+	struct value *res = calloc(1, sizeof *res);
+	res->kind = value_kind_pair;
+	res->u.pair[0] = eval(action->args[0]);
+	res->u.pair[1] = eval(action->args[1]);
+	return res;
+}
+
+static struct value *primitive_first(const struct action *action) {
+	struct value *pair = eval(action->args[0]);
+	if (pair && pair->kind == value_kind_pair) {
+		return pair->u.pair[0];
+	} else {
+		log_error("Cannot take first of non-pair.");
+		return 0;
+	}
+}
+
+static struct value *primitive_second(const struct action *action) {
+	struct value *pair = eval(action->args[0]);
+	if (pair && pair->kind == value_kind_pair) {
+		return pair->u.pair[1];
+	} else {
+		log_error("Cannot take second of non-pair.");
+		return 0;
+	}
+}
+
+static void write_value(struct value *v) {
 	switch (v->kind) {
 	case value_kind_string:
 		printf("%.*s", (int) v->u.string.length, v->u.string.data);
@@ -86,9 +115,17 @@ static struct value *primitive_write(const struct action *action) {
 	case value_kind_number:
 		printf("%lld", v->u.number);
 		break;
+	case value_kind_pair:
+		write_value(v->u.pair[0]);
+		printf(", ");
+		write_value(v->u.pair[1]);
+		break;
 	default:
 		log_error("Unrecognized value kind %d", v->kind);
 	}
+}
+static struct value *primitive_write(const struct action *action) {
+	write_value(eval(action->args[0]));
 	return 0;
 }
 
@@ -139,17 +176,16 @@ static struct value *primitive_not(const struct action *action) {
 	if (!v || v->kind != value_kind_boolean) {
 		log_error("not requires a boolean");
 		free(res);
+		return 0;
 	}
 	res->kind = value_kind_boolean;
 	res->u.boolean = !v->u.boolean;
 	return res;
 }
 
-static struct value *primitive_is_equal_to(const struct action *action) {
+static struct value *value_is_equal_to(struct value *x, struct value *y) {
 	struct value *res = calloc(1, sizeof *res);
 	if (res) {
-		struct value *x = eval(action->args[0]);
-		struct value *y = eval(action->args[1]);
 		res->kind = value_kind_boolean;
 		if (x->kind == y->kind) {
 			switch (x->kind) {
@@ -162,6 +198,10 @@ static struct value *primitive_is_equal_to(const struct action *action) {
 			case value_kind_number:
 				res->u.boolean = x->u.number == y->u.number;
 				break;
+			case value_kind_pair:
+				res->u.boolean = value_is_equal_to(x->u.pair[0], y->u.pair[0])
+					&& value_is_equal_to(x->u.pair[1], y->u.pair[1]);
+				break;
 			default:
 				log_error("Unrecognized kind %d in (x) is equal to (y).", x->kind);
 				res->u.boolean = false;
@@ -172,6 +212,12 @@ static struct value *primitive_is_equal_to(const struct action *action) {
 		}
 	}
 	return res;
+}
+
+static struct value *primitive_is_equal_to(const struct action *action) {
+	struct value *x = eval(action->args[0]);
+	struct value *y = eval(action->args[1]);
+	return value_is_equal_to(x, y);
 }
 
 static struct value *primitive_if_then_else(const struct action *action) {
@@ -367,6 +413,9 @@ static bool define_primitive(
 
 int main(int argc, char **argv) {
 	environment = 0;
+	struct definition pair_definition;
+	struct definition first_definition;
+	struct definition second_definition;
 	struct definition write_line_definition;
 	struct definition write_definition;
 	struct definition read_character_definition;
@@ -383,7 +432,10 @@ int main(int argc, char **argv) {
 	struct definition define_procedure_definition;
 	struct definition define_global_variable_definition;
 	struct definition set_definition;
-	if (!define_primitive(&write_line_definition, "write line (msg)", primitive_write_line)
+	if (!define_primitive(&pair_definition, "(first), (second)", primitive_pair)
+	    || !define_primitive(&first_definition, "first (pair)", primitive_first)
+	    || !define_primitive(&second_definition, "second (pair)", primitive_second)
+	    || !define_primitive(&write_line_definition, "write line (msg)", primitive_write_line)
 	    || !define_primitive(&write_definition, "write (msg)", primitive_write)
 	    || !define_primitive(&read_character_definition, "read character", primitive_read_character)
 	    || !define_primitive(&append_character_to_definition, "append character (c) to (s)", primitive_append_character_to)
