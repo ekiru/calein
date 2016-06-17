7b83549180c7b5156c0e288752756156f3c8156e
Tyler Curtis <ekiru.0@gmail.com>
2016-06-17 13:37:30 -0400

    Don't warn in compile.sh.

--- compile.sh
+++ compile.sh
@@ -2,5 +2,5 @@
 
 #./interp compiler.cln < "$1" > /tmp/calein-script.c &&
 ./compiler < "$1" > /tmp/calein-script.c &&
-cc -I . -o /tmp/calein-exe /tmp/calein-script.c err.c str.c value.c &&
+cc -w -I . -o /tmp/calein-exe /tmp/calein-script.c err.c str.c value.c &&
 /tmp/calein-exe
