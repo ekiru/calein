5ba3dd52a7783e314635293071fffe084dd0fd2d
Tyler Curtis <ekiru.0@gmail.com>
2016-07-09 15:23:33 -0500

    Teach compile.sh to understand -g.

--- compile.sh
+++ compile.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+cflags='-w -I .'
+executer=
+
+if [ $1 = '-g' ]
+then
+	shift
+	cflags="$cflags -g"
+	executer=lldb
+fi
+
 #./interp compiler.cln < "$1" > /tmp/calein-script.c &&
 ./compiler < "$1" > /tmp/calein-script.c &&
-cc -w -I . -o /tmp/calein-exe /tmp/calein-script.c err.c str.c value.c &&
-/tmp/calein-exe
+cc $cflags -o /tmp/calein-exe /tmp/calein-script.c err.c str.c value.c &&
+$executer /tmp/calein-exe
