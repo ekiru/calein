5d5c4d27c17ef4abdd187fad5cbafaa22d2ccee8
Tyler Curtis <ekiru.0@gmail.com>
2016-07-10 09:50:28 -0500

    Make source level debugging optional.

--- compile.sh
+++ compile.sh
@@ -11,6 +11,6 @@ then
 fi
 
 #./interp compiler.cln < "$1" > /tmp/calein-script.c &&
-./compiler "$1" > /tmp/calein-script.c &&
+./compiler -g "$1" > /tmp/calein-script.c &&
 cc $cflags -o /tmp/calein-exe /tmp/calein-script.c err.c str.c value.c &&
 $executer /tmp/calein-exe
